e676ee9959c75da975ab5cb0742c0246d9e3b953
htmlscratch <taiden358@gmail.com>
2026-02-06 20:11:34 -0600

    Update blocks.js

--- blocks.js
+++ blocks.js
@@ -1,16 +1,17 @@
-// HTML
+/** HTML BLOCKS **/
 Blockly.Blocks['html_main'] = {
   init: function() {
     this.appendDummyInput().appendField("HTML5 Document");
-    this.appendStatementInput("CONTENT").setCheck(null).appendField("body");
+    this.appendStatementInput("CONTENT").setCheck(null).appendField("body content:");
     this.setColour(160);
+    this.setTooltip("The root of your website.");
   }
 };
 
 Blockly.Blocks['html_text'] = {
   init: function() {
-    this.appendDummyInput().appendField("Text").appendField(new Blockly.FieldTextInput("Hello"), "TEXT");
-    this.appendStatementInput("STYLE").setCheck(null).appendField("style");
+    this.appendDummyInput().appendField("Paragraph Text").appendField(new Blockly.FieldTextInput("Hello World"), "TEXT");
+    this.appendStatementInput("STYLE").setCheck(null).appendField("css styles:");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(160);
@@ -20,17 +21,18 @@ Blockly.Blocks['html_text'] = {
 Blockly.Blocks['html_button'] = {
   init: function() {
     this.appendDummyInput().appendField("Button").appendField(new Blockly.FieldTextInput("Click Me"), "TEXT");
-    this.appendStatementInput("JS").setCheck(null).appendField("when clicked");
+    this.appendStatementInput("JS").setCheck(null).appendField("when clicked (js):");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(160);
   }
 };
 
-// CSS
+/** CSS BLOCKS **/
 Blockly.Blocks['css_color'] = {
   init: function() {
-    this.appendDummyInput().appendField("Text Color").appendField(new Blockly.FieldColour("#ff0000"), "COL");
+    const colorField = Blockly.FieldColour ? new Blockly.FieldColour("#4a90e2") : new Blockly.FieldTextInput("#4a90e2");
+    this.appendDummyInput().appendField("text color:").appendField(colorField, "COL");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(260);
@@ -39,17 +41,17 @@ Blockly.Blocks['css_color'] = {
 
 Blockly.Blocks['css_font_size'] = {
   init: function() {
-    this.appendDummyInput().appendField("Font Size").appendField(new Blockly.FieldTextInput("20px"), "SIZE");
+    this.appendDummyInput().appendField("font size:").appendField(new Blockly.FieldTextInput("24px"), "SIZE");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(260);
   }
 };
 
-// JS
+/** JS BLOCKS **/
 Blockly.Blocks['js_alert'] = {
   init: function() {
-    this.appendDummyInput().appendField("Alert Box").appendField(new Blockly.FieldTextInput("Hi!"), "MSG");
+    this.appendDummyInput().appendField("show alert:").appendField(new Blockly.FieldTextInput("Button Clicked!"), "MSG");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(60);
@@ -58,7 +60,7 @@ Blockly.Blocks['js_alert'] = {
 
 Blockly.Blocks['js_console'] = {
   init: function() {
-    this.appendDummyInput().appendField("Log to Console").appendField(new Blockly.FieldTextInput("Working..."), "MSG");
+    this.appendDummyInput().appendField("log to console:").appendField(new Blockly.FieldTextInput("Hello Console"), "MSG");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(60);
